84a7cb4619cd0f4e3aeac46436ae514d6078ce6b
waynemorphic <wayneklyde@gmail.com>
2022-07-01 20:55:17 +0300

    developed api endpoint and jwt token authorization

--- mkulima/settings.py
+++ mkulima/settings.py
@@ -38,10 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # third parties libraries
-    'phonenumber_field',
-    'location_field.apps.DefaultConfig',
+    'rest_framework',
 
     # own 
     'users',
@@ -70,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # for media
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -130,3 +129,13 @@ STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# jwt settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import home
+from django.urls import path, re_path
+from users import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home , name='home'),
+    re_path(r'^api/farmer/$', views.FarmerData.as_view()),
+    re_path(r'^api/buyer/$', views.BuyerData.as_view()),
+    re_path(r'^api/supplier/$', views.SupplierData.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
